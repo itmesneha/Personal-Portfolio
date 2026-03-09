50ec7ac12c2bf447a849409857a81b189ac8eed5
sneha <30837089+itmesneha@users.noreply.github.com>
2026-03-09 14:45:32 +0800

    fixed positioning and mobile displays, need navbar on mobile tho

--- pages/index.js
+++ pages/index.js
@@ -12,32 +12,42 @@ export default function Home() {
         {/* Scattered images positioned around the page */}
         <img
           src="/static/images/laptop.png"
-          height={120}
-          width={120}
-          className="absolute left-20 top-32 opacity-80"
-        />
+          height="50"
+          width="50"
+          className="absolute right-40 top-40 hidden opacity-80 lg:block"
+        ></img>
         <img
           src="/static/images/mail.png"
-          height={100}
-          width={100}
-          className="absolute right-32 top-20 opacity-80"
-        />
+          height="50"
+          width="50"
+          className="absolute left-64 bottom-24 hidden opacity-80 lg:block"
+        ></img>
         <img
           src="/static/images/mouse.png"
-          height={90}
-          width={90}
-          className="absolute left-32 bottom-24 opacity-80"
-        />
+          height="60"
+          width="60"
+          className="absolute right-0 bottom-20 hidden opacity-80 lg:block"
+        ></img>
 
         {/* Main content - stays centered */}
-        <div className="flex min-h-[80vh] items-center justify-center">
-          <div className="flex flex-col items-center text-center">
+        <div className="flex min-h-[80vh] flex-col items-center justify-center lg:flex-row">
+          <div className="mb-8 lg:mb-0">
+            {/* Mobile version - smaller */}
+            <div className="block lg:hidden">
+              <AnimatedAvatar width={200} height={200} fps={8} />
+            </div>
+            {/* Desktop version - larger */}
+            <div className="hidden lg:block">
+              <AnimatedAvatar width={300} height={300} fps={8} />
+            </div>
+          </div>
+          <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
             <h1 className="whitespace-nowrap pb-6 text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl md:text-6xl lg:text-7xl">
               Hi, I'm{' '}
               <span className="text-primary-color-500 dark:text-primary-color-dark-500">Sneha</span>
               .
             </h1>
-            <h2 className="prose max-w-3xl pt-5 text-base text-gray-600 dark:text-gray-300 md:text-lg lg:text-xl">
+            <h2 className="max-w-3xl pr-0 text-base text-gray-600 dark:text-gray-300 md:text-lg lg:text-xl">
               {`Welcome to ${siteMetadata.description}. I'm an AI/ML Engineer in Singapore, building intelligent systems that solve real problems.`}
             </h2>
             <p className="max-w-3xl pt-8 text-sm leading-6 text-slate-600 dark:text-slate-300 md:text-base">
@@ -54,9 +64,6 @@ export default function Home() {
                 reach out to chat!&nbsp;
               </RoughNotation>
             </p>
-          </div>
-          <div>
-            <AnimatedAvatar width={300} height={300} fps={8} />
           </div>
         </div>
       </div>
