7fb06cfe452369933988de7b558a0de48454503a
sneha <30837089+itmesneha@users.noreply.github.com>
2026-03-11 21:58:04 +0800

    removed contact from navbar

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/contact', title: 'Contact' },
+  // { href: '/contact', title: 'Contact' },
   { href: '/experience', title: 'Experience' },
 ]
 
